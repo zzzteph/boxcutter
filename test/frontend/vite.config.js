7263cfb98343453310f25e55bcc57229eb9e9870
zzzteph <stephanzag@gmail.com>
2026-06-12 19:28:16 +0200

    remove tests

--- test/frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// Built assets are served by Flask from /static/spa/.
-export default defineConfig({
-  plugins: [vue()],
-  base: '/static/spa/',
-  build: { outDir: 'dist', emptyOutDir: true }
-})
